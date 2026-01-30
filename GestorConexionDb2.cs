15c7527fcc8617e067908210bb9bbb51285e9986
Marcelo Molina <molinamarcelo113@gmail.com>
2026-01-29 23:44:55 -0600

    Implementar gestor de conexión DB2

---
+++ GestorConexionDb2.cs
@@ -0,0 +1,34 @@
+﻿using IBM.Data.Db2;
+using System.Data;
+
+namespace ProyectoDB2
+{
+    public class GestorConexionDb2
+    {
+        private string cadenaConexion;
+
+        public GestorConexionDb2()
+        {
+            cadenaConexion =
+                "Server=localhost:50000;" +
+                "Database=PROJDB;" +
+                "UID=db2inst1;" +
+                "PWD=contra;";
+        }
+
+        public DataTable EjecutarConsulta(string sql)
+        {
+            DB2Connection conexion = new DB2Connection(cadenaConexion);
+            DB2Command comando = new DB2Command(sql, conexion);
+            DB2DataAdapter adaptador = new DB2DataAdapter(comando);
+
+            DataTable tabla = new DataTable();
+
+            conexion.Open();
+            adaptador.Fill(tabla);
+            conexion.Close();
+
+            return tabla;
+        }
+    }
+}
